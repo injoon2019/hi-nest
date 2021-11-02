5229205766a0adc1716c59c80564537dd38c3ba6
Joon <joon@Joonui-MacBookPro.local>
2021-11-02 22:53:48 +0900

    2장

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { Movie } from './movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+    
+    constructor(private readonly moviesService: MoviesService) {}
+
+    @Get()
+    getAll(): Movie[]{
+        return this.moviesService.getAll();
+    }
+
+    @Get("/:id")
+    getOne(@Param('id') id: number): Movie {
+        return this.moviesService.getOne(id);
+    }
+
+    @Post()
+    create(@Body() movieData: CreateMovieDto) {
+        return this.moviesService.create(movieData);
+    }
+
+    @Delete("/:id")
+    remove(@Param('id') movieId:number) {
+        return this.moviesService.deleteOne(movieId);
+    }
+
+    @Patch("/:id")
+    patch(@Param('id') movieId: number, @Body() updateData) {
+        return this.moviesService.update(movieId, updateData);
+    }
+}
